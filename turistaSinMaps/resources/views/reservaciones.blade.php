16a6a7d35b69128eba8559a11613be2e312e43d1
Carol Chavez <122042076@upq.edu.mx>
2024-11-06 11:53:17 -0600

    Reservaciones

--- turistaSinMaps/resources/views/reservaciones.blade.php
+++ turistaSinMaps/resources/views/reservaciones.blade.php
@@ -4,10 +4,9 @@
 
 @vite(['resources/css/stylem.css'])
 
-
+<div class="container3">
 <h1 style="text-align: center">Reservaciones</h1>
-
-<br><div class="container3">
+ 
     <div class="left-box">
 
     <div class="rectangle">
@@ -28,13 +27,31 @@
 
     </table> <br>
     <button style="background-color : #d9534f; " > Cancelar reservación </button>
-           </div>  
-    </div>
-</div>
 
-<br><br>
-<div class="container3">
-<div class="rectangle">
+
+    <br><br>
+    <div class="left-box">
+
+    <div class="rectangle">
+       <table border="1" cellspacing="5" cellpadding="5">
+        <tr> 
+       <td> #Vuelo </td>   
+       <td> </td>          
+       <td> Fechas</td> 
+       <td> </td>        
+       <td>Precio total </td>
+    </tr> 
+
+    <tr> 
+       <td> Asiento </td>            
+       <td> </td>       
+       <td>Hora de compra </td>
+    </tr> 
+
+    </table> <br>
+    <button style="background-color : yellow; " > Editar reservación </button>
+
+   <br> <br> 
        <table border="1" cellspacing="5" cellpadding="5">
         <tr> 
        <td> #Vuelo </td>   
